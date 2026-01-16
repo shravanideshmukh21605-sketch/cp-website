31d82f94b405e07aaf76645359410ed167c28eff
shravanideshmukh21605-sketch <shravanideshmukh21605@gmail.com>
2026-01-16 12:14:47 +0530

    Added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,1 @@
-import { HeaderSimple } from './components/HeaderSimple';
 
-function App() {
-  return (
-    <>
-      <HeaderSimple />
-      <main>
-        Your page content
-      </main>
-    </>
-  );
-}
-
-export default App;
